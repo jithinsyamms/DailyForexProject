94aafcc4feeebfd4d8b58da657ad75ae4b4a88fd
jithin <jithinsyamms@gmail.com>
2021-12-27 14:32:16 +0530

    Initial Commit

---
+++ DailyForexProject/DailyForexProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyForexProjectApp.swift
+//  DailyForexProject
+//
+//  Created by Jithin on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DailyForexProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
